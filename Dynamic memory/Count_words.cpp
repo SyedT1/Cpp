04e9bb8ca9cd65e3ac92f9da6188a9f62abfcf3c
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-17 15:09:51 +0600

    Count words in the line taken as input

---
+++ Dynamic memory/Count_words.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void readline(char *&p){
+   	p = new char [100];
+    cin.getline(p,100);
+}
+void count_words(char *p,int *numofwords){
+	stringstream iterate(p);
+	for(*numofwords = 0;iterate>>p;(*numofwords)++);
+}
+int main(){
+	int numofw = 0;
+	char *c = nullptr;
+	readline(c);
+	count_words(c,&numofw);
+	cout<<"The total number of words in the input = "<<numofw<<'\n';
+	return 0;
+}
