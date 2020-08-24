05d034a178518b84401f1ed8074f51eca82309fe
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-24 21:21:32 +0600

    Storing all divisors of n using realloc

--- Dynamic memory/Realloc.cpp
+++ Dynamic memory/Realloc.cpp
@@ -1,16 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ul = unsigned long long int;
-int main() {
-	//freopen("input.txt", "r", stdin);
-	freopen("pol.txt", "w", stdout);
-	int x = 0, *p;
-	for (int i = 1; i <= 10; i++) {
-		if (10 % i == 0) {
-			p = (int *)realloc(p, (++x) * sizeof(int));
-			p[x - 1] = i;
+int main(){
+    int *p=NULL,*q=NULL, div = 0, n;cin>>n;
+	for (int i = 1; i <=n; i++) {
+		if (n % i == 0) {
+			div++;
+			q=(int *)realloc(p,div*(sizeof(int)));
+			if(q!=NULL){
+                p=q;
+                p[div-1]=i;
+			}
+			else{
+			    free(p);
+			}
 		}
+
 	}
-	for (int i = 0; i < x; i++)cout << p[i] << ' ';
-	free(p);
+	for (int i = 0; i < div; i++) {
+		cout << p[i] << ' ';
+	}
 }
