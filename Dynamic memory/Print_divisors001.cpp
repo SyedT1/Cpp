7396df06b2470563cd051bc51a4f106c4f3ae0f5
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-26 20:11:49 +0600

    Print Divisors 001

--- Dynamic memory/Print_divisors001.cpp
+++ Dynamic memory/Print_divisors001.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 int main() {
 	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	freopen("out.txt", "w", stdout);
 	int a; cin >> a;
-	int **p = NULL, k = 0, *q = (int *)calloc(a, sizeof(int));
+	int **p = nullptr, k = 0, *q = (int *)calloc(a, sizeof(int));
 	for (int i = 0; i < a ; i++) {
 		for (int j = 1, fj = i + 1; j <= fj; j++) {
 			if (fj % j == 0) {
 				q[i]++;
 			}
 		}
-		if (p == NULL) {
+		if (p == nullptr) {
 			p = (int **)malloc(sizeof(int *));
 		} else {
 			p = (int **)realloc(p, (i + 1) * sizeof(int));
