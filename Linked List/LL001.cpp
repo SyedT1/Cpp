0d753824fbfecd81317ad4285d601a3a9fe8cb75
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-28 10:25:42 +0600

    Linked List Basic 001

---
+++ Linked List/LL001.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+    int x;
+    node *next = nullptr;
+};
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+    node *p = new node;
+    p->x = 3;
+    node *q = new node;
+    q->x = 4;
+    node *r = new node;
+    r->x = 5;
+    p->next = q;
+    q->next = r;
+    cout << p->x << '\n' << p->next->x << '\n' << p->next->next->x;
+}
