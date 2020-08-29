f48f985288b54e0ec8db699bcd363f771a8960c1
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-08-29 19:09:48 +0600

    LL 006
    Adding new nodes

---
+++ Linked List/LL006.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+	int x;
+	node *next = nullptr;
+};
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("kala.txt", "w", stdout);
+	node *list = new node;
+	node *start = list;
+	int num; cin >> num;
+	for (int i = 1, p; i <= num; i++) {
+		cin >> p;
+		if (i == 1) {
+			list->x = p;
+			continue;
+		}
+		node *tmp = new node;
+		tmp->x = p;
+		list->next = tmp;
+		list = list->next;
+	}
+	while (start != nullptr) {
+		cout << start->x << '\n';
+		start = start->next;
+	}
+}
