8135bd3bd4b7b3ed5fc9c6ce9c4bfbf05426d60f
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-09-07 22:16:38 +0600

    String003
    substrings from r[0 to n-1]  to n-1 without erase function

---
+++ String/Introduction/003.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printchar(char a[]) {
+	cout << a << '\n';
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	char c[10] = "lessons";
+	for (int i = 0; c[i] != '\0'; i++) {
+		printchar(&c[i]);
+	}
+}
