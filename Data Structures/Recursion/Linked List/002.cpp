04a2f3f9018563531ace550a76a98337285f51e5
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-09-22 17:03:51 +0000

    002
    Print a List in reverse order

---
+++ Data Structures/Recursion/Linked List/002.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+	int x;
+	node *next = nullptr;
+};
+void insert(node *&k, int val) {
+	if (!k) {
+		k = new node;
+		k->x = val;
+		return;
+	}
+	insert(k->next, val);
+}
+void printval(node *k) {
+	if (!k) {
+		return;
+	}
+	printval(k->next);
+	cout << k->x << ' ';
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	node *puthi = nullptr;
+	for (int i = 0, take; i < 4; i++) {
+		cin >> take;
+		insert(puthi, take);
+	}
+	printval(puthi);
+	return 0;
+}
