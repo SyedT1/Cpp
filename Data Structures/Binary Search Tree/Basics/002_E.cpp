dcbaf21461951bd7ed15b0fdc854211ab42a4705
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-09-23 07:03:24 +0000

    BST 002_E
    Remove Nodes ( 4 Cases ) Revised

---
+++ Data Structures/Binary Search Tree/Basics/002_E.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node {
+    int x;
+    node *lft = nullptr, *rgt = nullptr;
+};
+void insert(node *&k, int val) {
+    if (!k) {
+        k = new node;
+        k->x = val;
+    }
+    else if (val > k->x) {
+        insert(k->rgt, val);
+    } else {
+        insert(k->lft, val);
+    }
+}
+int minValue(node *&k) {
+    if (!k->lft) {
+        int val = k->x;
+        if (k->rgt) {
+            k = k->rgt;
+        } else {
+            k = nullptr;
+        }
+        return val;
+    }
+    return minValue(k->lft);
+}
+void remove(int val, node *&k) {
+    if (k == nullptr) {
+        cout << "Couldn't find the value to be removed\n";
+        return;
+    }
+    else if (k->x == val) {
+        /*Condition#1
+         where the searched node's left and right node is null
+         To do:
+         Just Nullify it
+         */
+        if ((!k->lft) && (!k->rgt)) {
+            k = nullptr;
+            //cout << "Condition 1 applied \n";
+            return;
+        }
+        /*Condition#2
+        where the searched node's left node is not nullptr but right is
+        To do:
+        X = X-lft;
+        */
+        else if (k->rgt == nullptr && k->lft != nullptr) {
+            k = k->lft;
+            //cout << "Condition 2 applied \n";
+            return;
+        }
+        /*Condition#3
+        where the searched node's left node is nullptr but right is not nullptr
+        To do:
+        X = X-rgt
+        */
+        else if (k->rgt != nullptr && k->lft == nullptr) {
+            //cout << "Condition 3 applied\n";
+            k = k->rgt;
+            return;
+        }
+        /*Condition#4
+        where the searched node's left node and right node is not null
+        To do:
+        */
+        else {
+            k->x = minValue(k->rgt);
+            //cout << "Condition 4 applied \n";
+            return;
+        }
+    }
+    else if (val > k->x) {
+        remove(val, k->rgt);
+    } else {
+        remove(val, k->lft);
+    }
+}
+void printTree(node *s) {
+    if (!s) {
+        return;
+    }
+    printTree(s->lft);
+    cout << s->x << ' ';
+    printTree(s->rgt);
+}
+int main() {
+    freopen("output.txt", "w", stdout);
+    node *k = nullptr;
+    int arr[] = { 4, 1, -3, 3, 13, 6, 8, 16, 15, 17};
+    for (int i = 0; i < 10; i++) {
+        insert(k, arr[i]);
+    }
+    /*
+          Tree formation
+
+                4
+           1              13
+       -3     3       6       16
+      N  N   N N     N 8   15    17
+                          N  N  N  N
+                  */
+    remove(4, k);
+    printTree(k);
+    return 0;
+}
