ed1ec817e60a9f7739dde9b70cb1ed16d8a72aec
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-09-24 04:18:10 +0000

    BST 006
    Depth of a node

---
+++ Data Structures/Binary Search Tree/Basics/006.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct bst {
+	int x;
+	bst *lft = nullptr, *rgt = nullptr;
+};
+void insert(bst *&q, int val) {
+	if (!q) {
+		q = new bst;
+		q->x = val;
+		return;
+	}
+	else if (val > q->x) {
+		insert(q->rgt, val);
+	} else {
+		insert(q->lft, val);
+	}
+}
+int depthfinder(bst *q, int i) {
+	if (!q) {
+		return -1;
+	} else if (q->x == i) {
+		return 0;
+	} else if (i > q->x) {
+		return depthfinder(q->rgt, i) + 1;
+	} else {
+		return depthfinder(q->lft, i) + 1;
+	}
+}
+int main() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int n; cin >> n;
+	bst *k = nullptr;
+	for (int i = 0, po; i < n; i++) {
+		cin >> po;
+		insert(k, po);
+	}
+	cout << depthfinder(k, 6) << '\n';
+	return 0;
+}
