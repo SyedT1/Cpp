1d0d0f70e1099d7747dd4d1ac835359a33386d00
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-10-09 21:07:52 +0600

    Passing three parameters

--- Algorithms/Recursion/W3/002/1.cpp
+++ Algorithms/Recursion/W3/002/1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 void fibo(int x, int y, int h) {
 	if (h <= 10) {
-		cout << x << ' ';
+		cout << y << ' ';
 		fibo(y, x + y, h + 1);
 	}
 	return;
 }
 int main() {
-	fibo(0, 1, 0);
+	fibo(0, 1, 1);
 }
