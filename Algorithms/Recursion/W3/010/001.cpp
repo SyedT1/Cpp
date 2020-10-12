66547f1ea7c34fc387f53f4a2eb3160f96ce19ef
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2020-10-12 22:35:38 +0600

    001( return int type)

--- Algorithms/Recursion/W3/010/001.cpp
+++ Algorithms/Recursion/W3/010/001.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 using namespace std;
 int binary(int x) {
-	if (x == 1) {
-		return 1;
+	if (x <= 1) {
+		return x;
 	}
 	return binary(x / 2) * 10 + (x % 2);
 }
 int main() {
-	cout << binary(4) << '\n';
+	cout << binary(8) << '\n';
 }
