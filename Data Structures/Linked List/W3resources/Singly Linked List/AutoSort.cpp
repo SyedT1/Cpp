a416053b3b0704a843efe63495c9f4c1040f3c1a
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-03-03 21:57:08 +0600

    AutoSort

--- Data Structures/Linked List/W3resources/Singly Linked List/AutoSort.cpp
+++ Data Structures/Linked List/W3resources/Singly Linked List/AutoSort.cpp
@@ -1,34 +1,34 @@
 #include<iostream>
 using namespace std;
-class Node{ 
- public:
-   int value;
-   Node *next = nullptr;
-   void ins(Node *&k,int v){
-	   if(k==nullptr || v < k->value){
-		   Node *temp = new Node{v,k};
-		   k = temp;
-           return;
-	   }
-       Node *temp = new Node{v,nullptr},*lo = k;
-	   while(lo->next!=nullptr && temp->value > lo->next->value){
-          lo = lo->next;
-	   }
-	   Node *lead = lo, *to = lo->next;
-	   temp->next = to;
-	   lead->next = temp;
-	   return;
-   }
-};
-int main(){
-	Node *link = nullptr;
-	int ar[]={1,-1,-2,-3,-4,3,2,1,-333,44,5555,20};
-	int nar = sizeof(ar)/sizeof(int);
-	for(int i = 0;i<nar;i++){
-      link->ins(link,ar[i]);
+class Node {
+public:
+	int value;
+	Node *next = nullptr;
+	void ins(Node *&k, int v) {
+		if (k == nullptr || v < k->value) {
+			Node *temp = new Node{v, k};
+			k = temp;
+			return;
+		}
+		Node *temp = new Node{v, nullptr}, *lo = k;
+		while (lo->next != nullptr && temp->value > lo->next->value) {
+			lo = lo->next;
+		}
+		Node *lead = lo, *to = lo->next;
+		temp->next = to;
+		lead->next = temp;
+		return;
 	}
-	while(link){
-		cout<<link->value<<' ';
+};
+int main() {
+	Node *link = nullptr;
+	int ar[] = {1, -1, -2, -3, -4, 3, 2, 1, -333, 44, 5555, 20};
+	int nar = sizeof(ar) / sizeof(int);
+	for (int i = 0; i < nar; i++) {
+		link->ins(link, ar[i]);
+	}
+	while (link) {
+		cout << link->value << ' ';
 		link = link->next;
 	}
 }
